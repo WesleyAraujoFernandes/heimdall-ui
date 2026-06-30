8af342cde9709e7d16573f88018239eb7f6789bc
wesleyfernandesprojetofaculdade <wesley@viaapia.com.br>
2026-06-30 17:59:06 -0300

    aling fields in browser

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SettingsService } from '../../services/settings.service';
+import { CollectionPreviewComponent } from "../collection-preview/collection-preview.component";
 @Component({
   selector: 'app-settings',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CollectionPreviewComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
@@ -25,17 +26,18 @@ export class SettingsComponent {
   ]
 
   form = this.fb.group({
-    collectionType: ['', Validators.required],
-    dataSource: ['', Validators.required],
-    dbms: ['', Validators.required],
-    databaseServerUrl: ['', Validators.required],
-    databaseServerPort: ['', Validators.required],
-    databaseName: ['', Validators.required],
-    databaseUsername: ['', Validators.required],
-    databasePassword: ['', Validators.required],
-    databaseTableName: ['', Validators.required],
-    databaseIdColumn: ['', Validators.required],
-    databaseQuery: ['', Validators.required],
+    collectionType: ['DATABASE', Validators.required],
+    dataSource: ['SQLSERVER', Validators.required],
+    dbms: ['SQLSERVER', Validators.required],
+    databaseServerUrl: ['10.1.1.27', Validators.required],
+    databaseServerPort: ['1433', Validators.required],
+    databaseName: ['CHRONUS_CEUB_2025', Validators.required],
+    databaseUsername: ['ChronusCEUB', Validators.required],
+    databasePassword: ['ChronusCEUB', Validators.required],
+    databaseTableName: ['TITULO', Validators.required],
+    databaseIdColumn: ['ID_TITULO', Validators.required],/*
+    databaseQuery: ['select id_titulo, str_titulo_alfabetado titulo_alfabetado, str_titulo titulo, a.str_nome_autorizado nome_autorizado from titulo t, autor a where t.id_autor = a.id_autor', Validators.required],*/
+    filesystemPath: ['\\\\10.1.1.4\\NASShare2\\compartilhamentos', Validators.required],
     databaseResultsPerPage: [10]
   })
 
